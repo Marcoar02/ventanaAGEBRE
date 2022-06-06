7ff42db37589a882f8e8e35f0e822bc439c3990c
Marcoar02 <marcoar0298@gmail.com>
2022-06-06 00:01:36 -0500

    cambios 06.06.22

--- main.js
+++ main.js
@@ -3,53 +3,64 @@ const dropdownMenu = document.querySelector(".dropdown-menu");
 
 const firstOption = document.querySelector(".first-option");
 const secondOption = document.querySelector(".second-option");
-const threeOption = document.querySelector(".three-option");
 
 dropdownButton.addEventListener("click",()=>{
   dropdownMenu.classList.toggle("dropdown-menu--active");
 });
+
+const options = document.querySelector(".options")
 
 dropdownMenu?.addEventListener("click",(event)=>{
   const target = event.target;
   const li = target.closest("li");
   firstOption.style.display = "none";
   secondOption.style.display = "none";
-  threeOption.style.display = "none";
+  document.querySelectorAll("[data-result]").forEach(element=>element.style.display='none')
+
+  let buttons = firstOption.querySelectorAll("button");
+  buttons.forEach(btn=>btn.classList.remove("option--active"));
+  buttons[0].classList.add("option--active")
   
+  buttons = secondOption.querySelectorAll("button");
+  buttons.forEach(btn=>btn.classList.remove("option--active"));
+  buttons[0].classList.add("option--active")
+
+  dropdownMenu.classList.remove("dropdown-menu--active");
   if (li.dataset["option"] === "first-option"){
     firstOption.style.display = "flex";
-
+    document.querySelector("[data-result='inicial']").style.display="block";
   }else if(li.dataset["option"] === "second-option"){
     secondOption.style.display = "flex";
-  }else{
-    threeOption.style.display = "flex";
+    document.querySelector("[data-result='inicial']").style.display="none";
   }
+});
+
+options.addEventListener("click",event=>{
+  const target = event.target;
+  const button = target.closest("button");
+  if(!button) return
+  const buttons = options.querySelectorAll("button");
+  buttons.forEach(btn=>btn.classList.remove("option--active"));
+  document.querySelectorAll("[data-result]").forEach(element=>element.style.display='none');
+  button.classList.add("option--active");
+  document.querySelector(`[data-result='${button.dataset['target']}']`).style.display='block';
 })
 
-firstOption?.addEventListener("click",event=> {
-  console.log("asdasd")
-  const target = event.target
-  const button = target.closest("button")
-  if(!button) return
-  const buttons = firstOption.querySelectorAll("button")
-  buttons.forEach(btn=>btn.classList.remove("option--active"))
-  button.classList.add("option--active")
-})
 
-secondOption?.addEventListener("click",event=> {
-  const target = event.target
-  const button = target.closest("button")
-  if(!button) return
-  const buttons = secondOption.querySelectorAll("button")
-  buttons.forEach(btn=>btn.classList.remove("option--active"))
-  button.classList.add("option--active")
-})
+// firstOption?.addEventListener("click",event=> {
+//   const target = event.target;
+//   const button = target.closest("button");
+//   if(!button) return;
+//   const buttons = firstOption.querySelectorAll("button");
+//   buttons.forEach(btn=>btn.classList.remove("option--active"));
+//   button.classList.add("option--active");
+// });
 
-threeOption?.addEventListener("click",event=> {
-  const target = event.target
-  const button = target.closest("button")
-  if(!button) return
-  const buttons = threeOption.querySelectorAll("button")
-  buttons.forEach(btn=>btn.classList.remove("option--active"))
-  button.classList.add("option--active")
-})
+// secondOption?.addEventListener("click",event=> {
+//   const target = event.target;
+//   const button = target.closest("button");
+//   if(!button) return;
+//   const buttons = secondOption.querySelectorAll("button");
+//   buttons.forEach(btn=>btn.classList.remove("option--active"));
+//   button.classList.add("option--active");
+// })
